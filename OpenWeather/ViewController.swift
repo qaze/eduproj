1bce0609b67f5fcf5ab00f0a7c062253fb0899f7
Nik Rodionov <rodionov.nikolay@gmail.com>
2020-01-27 20:43:14 +0300

    Initial Commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by Nik Rodionov on 27.01.2020.
+//  Copyright © 2020 nrodionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
